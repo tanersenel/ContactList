943669dd826ccf4c51428e124f83c7be2fd1792c
Taner ŞENEL <tanercat@hotmail.com>
2021-12-20 10:24:22 +0300

    Add project files.

---
+++ Contactlist.Contacts/Controllers/ContactController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contactlist.Contacts.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class ContactController : ControllerBase
+    {
+        
+    }
+}
---
+++ Contactlist.Contacts/Entities/Contact.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Contactlist.Contacts.Entities
+{
+    public class Contact
+    {
+        [BsonId]
+        public int UUID { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Firma { get; set; }
+        public List<ContactDetail> IletisimBilgileri { get; set; }
+    }
+    public class ContactDetail
+    {
+        public int BilgiTipi { get; set; }
+        public string BilgiIcerigi { get; set; }
+       
+    }
+}
