672bcef170e84d4a3f42ad90493e345ace38c20a
Taner ŞENEL <tanercat@hotmail.com>
2021-12-22 09:39:01 +0300

    Added Unit Test

---
+++ Contactlist.Contacts/Data/ContactContextSeedTests.cs
@@ -0,0 +1,39 @@
+﻿using Contactlist.Contacts.Data;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+
+namespace Contactlist.Contacts.Data
+{
+    [TestFixture]
+    public class ContactContextSeedTests
+    {
+
+
+        [SetUp]
+        public void SetUp()
+        {
+
+        }
+
+        private ContactContextSeed CreateContactContextSeed()
+        {
+            return new ContactContextSeed();
+        }
+
+        [Test]
+        public void SeedData_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var contactContextSeed = this.CreateContactContextSeed();
+            IMongoCollection contactCollection = null;
+
+            // Act
+            contactContextSeed.SeedData(
+                contactCollection);
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
---
+++ Contactlist.WebApp/Consumers/EventBusReportConsumerTests.cs
@@ -0,0 +1,51 @@
+﻿using Contactlist.WebApp.Consumers;
+using EventBusRabbitMQ;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Contactlist.WebApp.Consumers
+{
+    [TestFixture]
+    public class EventBusReportConsumerTests
+    {
+        private IRabbitMQPersistantConnection subRabbitMQPersistantConnection;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.subRabbitMQPersistantConnection = Substitute.For<IRabbitMQPersistantConnection>();
+        }
+
+        private EventBusReportConsumer CreateEventBusReportConsumer()
+        {
+            return new EventBusReportConsumer(
+                this.subRabbitMQPersistantConnection);
+        }
+
+        [Test]
+        public void Consume_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var eventBusReportConsumer = this.CreateEventBusReportConsumer();
+
+            // Act
+            eventBusReportConsumer.Consume();
+
+            // Assert
+            Assert.Fail();
+        }
+
+        [Test]
+        public void Disconnect_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var eventBusReportConsumer = this.CreateEventBusReportConsumer();
+
+            // Act
+            eventBusReportConsumer.Disconnect();
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
